5b6139d275db0ac213de070645494d509a541d8f
Dimitar Dimitrov <dimitar.dimitrov@gmail.com>
2018-09-30 21:38:15 +0900

    Added sed-like property manipulation and diagnostic options

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ altogether and build with a locally installed Gradle distribution.
 Now there is another way! The `gwo-agent` allows you to override or filter any property specified in `gradle.properties`
 giving you extra control over the build distribution, without modifying the checked out sources.
 
+## Basic usage
+
 All you need is to download the latest version  [![Release](https://jitpack.io/v/ddimtirov/gwo-agent.svg)](https://jitpack.io/#ddimtirov/gwo-agent) or use `curl`:
 
 ````bash
@@ -32,3 +34,42 @@ gradlew clean pub
 
 ***Note:*** If `GRADLE_OPTS` is already used by your build, you may as well use `JAVA_TOOL_OPTIONS` and `_JAVA_OPTIONS`, 
 though keep in mind these will impact any other Java process.
+
+## Advanced usage
+
+Often you would want to edit a small part of a property. For example:
+ 
+````bash
+# replace a blacklisted Gradle version with a good one
+set GRADLE_OPTS=-javaagent:build/gwo-agent.jar=distributionUrl~=/4.10-bin/4.10.2-bin/
+
+# use a specific Gradle version for all distributions, keeping the bin/all classifier
+set GRADLE_OPTS=-javaagent:build/gwo-agent.jar=distributionUrl~=/-([0-9\.]+)-(bin|all)/4.10-$2/
+
+# use same version from alternative download site
+set GRADLE_OPTS=-javaagent:build/gwo-agent.jar=distributionUrl~=@https://services.gradle.org/distributions@https://mymirror/gradle/@
+````
+
+In summary, the syntax for property transformation is:
+
+* separate the property-key and transform definition with `~=` (instead of `=`)
+* the first character of the transform definition is treated as delimiter. It is similar to `sed s/pattern/replace/flags`, except we don't support flags.
+* The pattern and replacement use the Java regex syntax.
+
+## Debug options
+
+Especially with the transform options, sometimes we may want to get extra insight in what the agent is doing.
+
+Set the `gwo.debug.resolution` JVM property to `true` to dump details about the properties which are handled by the agent.
+
+````bash
+> set GRADLE_OPTS=-Dgwo.debug.resolution=true -javaagent:build/gwo-agent.jar=distributionUrl~=/4.10.2-bin/4.10-bin/,distributionSha256Sum=abc123
+
+> gradlew --version
+Transformed Gradle Wrapper property: distributionUrl = 'https://services.gradle.org/distributions/gradle-4.10.2-bin.zip' -> 'https://services.gradle.org/distributions/gradle-4.10-bin.zip'
+Overrode Gradle Wrapper property: distributionSha256Sum = 'abc123'
+````
+
+Finally, if you need to check whether the agent is instrumenting the right classes, set 
+the `gwo.debug.instrumentation` JVM property to `true` to dump details about ByteBuddy's 
+class inspection and instrumentation.
--- src/main/java/io/github/ddimtirov/gradle/wrapperoverride/GradleWrapperOverrideAgent.java
+++ src/main/java/io/github/ddimtirov/gradle/wrapperoverride/GradleWrapperOverrideAgent.java
@@ -4,8 +4,8 @@ import net.bytebuddy.agent.builder.AgentBuilder;
 import net.bytebuddy.asm.Advice;
 
 import java.lang.instrument.Instrumentation;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.regex.Pattern;
 
 import static net.bytebuddy.matcher.ElementMatchers.named;
 import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
@@ -13,18 +13,49 @@ import static net.bytebuddy.matcher.ElementMatchers.takesArguments;
 @SuppressWarnings({"unused", "UnusedAssignment", "ParameterCanBeLocal", "SameParameterValue", "WeakerAccess"})
 public class GradleWrapperOverrideAgent {
     public static final boolean DEBUG_INSTRUMENTATION = Boolean.getBoolean("gwo.debug.instrumentation");
+    public static final boolean DEBUG_RESOLUTION = Boolean.getBoolean("gwo.debug.resolution");
+
     public static Map<String, String> overrides = new HashMap<String, String>();
+    public static Map<String, ValueTransformer> transforms = new HashMap<String, ValueTransformer>();
     public static synchronized void premain(String arguments, Instrumentation instrumentation) {
         Map<String, String> parsed = new HashMap<String, String>();
         for (String pair : arguments.split(",")) {
             String[] kv = pair.split("=", 2);
+            String key = kv[0].trim();
             if (kv.length == 1) {
-                if (kv[0].trim().length() == 0) continue;
+                if (key.length() == 0) continue;
                 throw new IllegalArgumentException("Malformed pair '" + pair + "'. Format is: 'key=value" + "," + "key=value...' (NOTE: both key and value are trimmed)");
             }
+            String value = kv[1].trim();
 
-            // TODO: add syntax for filtering existing props: key~=/REGEX/REPLACE/
-            parsed.put(kv[0].trim(), kv[1].trim());
+            if (key.endsWith("~")) {
+                String tKey = key.substring(0, key.length() - 1);
+                if (!transforms.containsKey(tKey)) transforms.put(tKey, new ValueTransformer());
+                ValueTransformer transformer = transforms.get(tKey);
+
+                // VALUE FORMAT: /REGEX/REPLACE/ where you can use any other char instead of slash, as long as it does not appear in the text
+                String separator = String.valueOf(value.charAt(0));
+                String[] parts = value.split(Pattern.quote(separator), -1);
+
+                String err = null;
+                if (parts.length!=4) {
+                    err = "Transform pattern has format: /foo(.*)bar/baz\\1qux/ OR @foo(.*)bar@baz\\1qux@ OR /-4.10-all/-4.10.2-bun/";
+                }
+                if (err==null && !(parts[0].isEmpty() && parts[3].isEmpty())) err = "The transform pattern can not have text before and after the leading/trailing separator";
+                if (err!=null) {
+                    System.err.println(err);
+                    System.err.println("GOT: '" + value +"' -> " + Arrays.toString(parts));
+                    System.exit(1);
+                }
+
+                String regex = parts[1];
+                String replace = parts[2];
+                Pattern pattern = Pattern.compile(regex);
+
+                transformer.addTransform(pattern, replace);
+            } else {
+                parsed.put(key, value);
+            }
         }
         overrides.putAll(parsed);
 
@@ -33,10 +64,32 @@ public class GradleWrapperOverrideAgent {
         if (DEBUG_INSTRUMENTATION) {
             builder = builder.with(AgentBuilder.Listener.StreamWriting.toSystemError());
         }
+
+        // https://github.com/gradle/gradle/blob/master/subprojects/wrapper/src/main/java/org/gradle/wrapper/WrapperExecutor.java#L119
         builder.type(named("org.gradle.wrapper.WrapperExecutor")).transform(adviser.advice(
                 named("getProperty").and(takesArguments(3)),
                 GradleWrapperOverrideAgent.class.getName() + "$OverrideProperty"
         )).installOn(instrumentation);
+    }
+
+    public static class ValueTransformer {
+        List<Pattern> patterns = new ArrayList<Pattern>();
+        List<String> replaces = new ArrayList<String>();
+
+        void addTransform(Pattern pattern, String replace) {
+            patterns.add(pattern);
+            replaces.add(replace);
+        }
+
+        public String transformValue(String value) {
+            String current = value;
+            for (int i = 0; i < patterns.size(); i++) {
+                Pattern pattern = patterns.get(i);
+                String replace = replaces.get(i);
+                current = pattern.matcher(current).replaceAll(replace);
+            }
+            return current;
+        }
     }
 
     static class OverrideProperty {
@@ -46,8 +99,22 @@ public class GradleWrapperOverrideAgent {
         }
 
         @Advice.OnMethodExit()
-        static void dumpException(@Advice.Enter final String override, @Advice.Return(readOnly = false) String retval) {
-            if (override!=null) retval=override;
+        static void dumpException(
+                @Advice.Argument(0) final String propName,
+                @Advice.Enter final String override, @Advice.Return(readOnly = false) String retval
+        ) {
+            if (override != null) {
+                if (DEBUG_RESOLUTION) System.err.printf("Overrode Gradle Wrapper property: %s = '%s'%n", propName, override);
+                retval = override;
+                return;
+            }
+
+            ValueTransformer transformer = transforms.get(propName);
+            if (transformer!=null) {
+                String transformed = transformer.transformValue(retval);
+                if (DEBUG_RESOLUTION) System.err.printf("Transformed Gradle Wrapper property: %s = '%s' -> '%s'%n", propName, retval, transformed);
+                retval = transformed;
+            }
         }
     }
 
